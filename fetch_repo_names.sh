b038736845d8f9a2878a4abef9b3af48ff93f6cb
Julian Holfeld <julianholfeld@student.uni-kassel.de>
2020-11-15 14:17:52 +0100

    fix: empty page count will now set the page_count to 1

--- fetch_repo_names.sh
+++ fetch_repo_names.sh
@@ -5,6 +5,11 @@ PREFIX=$2
 
 PAGE_COUNT=$(curl -sI -H "Authorization: token $GITHUB_TOKEN" "https://api.github.com/orgs/$ORG/repos" | sed -n -r 's/^link:.*<[^>]*page=([0-9]+)>; rel="last".*$/\1/ip')
 
+if [ -z "$PAGE_COUNT" ]
+then 
+    PAGE_COUNT=1
+fi
+
 for ((i=1;i<=PAGE_COUNT;i++)); do
     curl -s \
     -H "Authorization: token $GITHUB_TOKEN" \
